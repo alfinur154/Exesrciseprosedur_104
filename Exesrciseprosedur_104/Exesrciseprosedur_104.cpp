7cae4a9a9f07e42c56e5862ae9fa3f48698a3591
alfinur154 <alfinur27@gmail.com>
2023-03-27 10:45:12 +0700

    exescise

--- Exesrciseprosedur_104/Exesrciseprosedur_104.cpp
+++ Exesrciseprosedur_104/Exesrciseprosedur_104.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+double rerata(double a, double b) {
+	return (a + b) / 2;
+}
+
+string status(double rata) {
+	if (rata >= 70)
+		return "Selamat Anda Lulus";
+	else
+		return "Maaf Anda Gagal";
+}
+string status2(double rata, double nil) {
+	if (rata >= 70 || nil >= 80)
+		return "Selamat Anda Lulus";
+	else
+		return "Maaf Anda Gagal";
+}
+string nama;
+int j:
+
+void input() {
+	for (int i = 0; i < 20; i++) {
+		cout << "masukkan nama = ";
+	}
+}	return
+
+
+
+int main() {
+	double nilm, nilB;
+	cout << "masukkan nama kandidat = ";
+	cin >> nama;
+	cout << "masukkan nilai mtk = ";
+	cin >> nilm;
+	cout << "masukkan nilai bahasa inggris = ";
+	cin >> nilB;
+	cout << "status kelulusan = " << status(rerata(nilm, nilB));
+	cout << "\nstatus kelulusan ke 2 = " << status2(rerata(nilm, nilB));
+
+
 }
